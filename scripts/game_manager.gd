7591d7b8cc21d9178e4c76b8f20e3358d9ef3b27
Radhika-15-Agarwal <24ucs128@lnmiit.ac.in>
2026-02-03 12:27:37 +0530

    Added Game Over UI

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,6 +9,10 @@ func game_over():
 	
 	is_game_over = true
 	print("Game Over")
+	
+	var ui_label = get_tree().get_first_node_in_group("game_over_label")
+	if ui_label:
+		ui_label.visible = true
 
 func restart_game():
 	is_game_over = false
